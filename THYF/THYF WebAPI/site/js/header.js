fab4ab71562fccb58c221bb51c58daa0c865b9ba
haloskzd <haloskzd@HALOSKZD-W530.rose-hulman.edu>
2016-02-02 02:02:35 -0500

    Temp header updates, will use another page when I can figure it out

---
+++ THYF/THYF WebAPI/site/js/header.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function ()
+{
+	$("#logout-link").on("click", function (e)
+	{
+		$.ajax({
+			type: "DELETE",
+			url: "/api/Login",
+			contentType: "application/json",
+			datatype: "json"
+		}).done(function ()
+		{
+			$("#logged-in").text("");
+			$("#logged-in-block").hide();
+			$("#notlogged-in-block").show();
+			alert("Logged out successfully!");
+		});
+	});
+	var logoutLabels = function ()
+	{
+		$.ajax({
+			type: "DELETE",
+			url: "/api/Login",
+			contentType: "application/json",
+			datatype: "json"
+		}).done(function ()
+		{
+			$("#logged-in").text("");
+			$("#logged-in-block").hide();
+			$("#notlogged-in-block").show();
+			alert("Logged out successfully!\nReturned: '" + data + "'");
+		});
+	};
+	THYF.hideLoading();
+});
