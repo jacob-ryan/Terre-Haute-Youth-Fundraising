38259850e4cd181dd35b874a0912bf1ddae80530
haloskzd <haloskzd@HALOSKZD-W530.rose-hulman.edu>
2016-02-02 04:21:10 -0500

    Updating jQuery to add validation for login
    Does not work yet.  Still taking a look at why the validation doesn't
    seem to be submitting.

--- THYF/THYF WebAPI/site/js/login.js
+++ THYF/THYF WebAPI/site/js/login.js
@@ -1,25 +1,44 @@
 ﻿$(document).ready(function()
     {
-        $("#login-button").on("click", function(e)
-        {
-            e.preventDefault();
-            var data = {
-                email: $("#email").val(),
-                password: $("#password").val(),
-                rememberMe: true
-            };
-        	$.ajax({
-        		type: "POST",
-                url: "/api/Login",
-                contentType: "application/json",
-				data: data ? JSON.stringify(data) : null,
-				datatype: "json"
-        }).done(function(data)
-        {
-        	alert("Logged in successfully!\nReturned: '" + data + "'");
-        	loginLabels();
-        });
-        });
+	$("#login-button").on("click", function(e)
+	{
+		//e.preventDefault();
+		$("#login-form").validate(
+			{
+				submitHandler: function ()
+				{
+					submitLogic();
+				},
+				errorClass: "help-block animation-slideDown",
+				errorElement: "div",
+				errorPlacement: function (error, e)
+				{
+					e.parents(".form-group > div").append(error);
+				},
+				highlight: function (e)
+				{
+					$(e).closest(".form-group").removeClass("has-success has-error").addClass("has-error");
+					$(e).closest(".help-block").remove();
+				},
+				success: function (e)
+				{
+					e.closest(".form-group").removeClass("has-success has-error");
+					e.closest(".help-block").remove();
+				},
+				rules: {
+					"email": {
+						required: true,
+						maxlength: 255,
+						email: true
+					},
+					"password": {
+						required: true,
+						maxlength: 255
+					}
+				}
+			});
+		alert("validation code");
+	});
         var loginLabels = function ()
         {
         	alert("users");
@@ -33,6 +52,25 @@
         		$("#logged-in").text("User:" + user.name);
         		$("#logged-in-block").show();
         		$("#notlogged-in-block").hide();
+        	});
+        };
+        var submitLogic = function ()
+        {
+        	var data = {
+        		email: $("#email").val(),
+        		password: $("#password").val(),
+        		rememberMe: true
+        	};
+        	$.ajax({
+        		type: "POST",
+        		url: "/api/Login",
+        		contentType: "application/json",
+        		data: data ? JSON.stringify(data) : null,
+        		datatype: "json"
+        	}).done(function (data)
+        	{
+        		alert("Logged in successfully!\nReturned: '" + data + "'");
+        		loginLabels();
         	});
         };
         THYF.hideLoading();
