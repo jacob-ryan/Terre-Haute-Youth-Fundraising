a7f069b66d864e75677f24e53130ea955630c39d
Jacob Ryan <ryanjm@rose-hulman.edu>
2016-02-02 11:20:14 -0500

    Fixed login validation, improved HTML and styling of home page

--- THYF/THYF WebAPI/site/js/login.js
+++ THYF/THYF WebAPI/site/js/login.js
@@ -1,77 +1,59 @@
 ﻿$(document).ready(function()
-    {
-	$("#login-button").on("click", function(e)
+{
+	$("#login-form").validate(
 	{
-		//e.preventDefault();
-		$("#login-form").validate(
-			{
-				submitHandler: function ()
-				{
-					submitLogic();
-				},
-				errorClass: "help-block animation-slideDown",
-				errorElement: "div",
-				errorPlacement: function (error, e)
-				{
-					e.parents(".form-group > div").append(error);
-				},
-				highlight: function (e)
-				{
-					$(e).closest(".form-group").removeClass("has-success has-error").addClass("has-error");
-					$(e).closest(".help-block").remove();
-				},
-				success: function (e)
-				{
-					e.closest(".form-group").removeClass("has-success has-error");
-					e.closest(".help-block").remove();
-				},
-				rules: {
-					"email": {
-						required: true,
-						maxlength: 255,
-						email: true
-					},
-					"password": {
-						required: true,
-						maxlength: 255
-					}
-				}
-			});
-		alert("validation code");
+		submitHandler: function()
+		{
+			alert("Submitting form...");
+			submit();
+		},
+		rules: {
+			"email": {
+				required: true,
+				maxlength: 255,
+				email: true
+			},
+			"password": {
+				required: true,
+				maxlength: 255
+			}
+		}
 	});
-        var loginLabels = function ()
-        {
-        	alert("users");
-        	$.ajax({
-        		type: "GET",
-        		url: "/api/Login",
-        		contentType: "application/json",
-        		datatype: "json"
-        	}).done(function (user)
-        	{
-        		$("#logged-in").text("User:" + user.name);
-        		$("#logged-in-block").show();
-        		$("#notlogged-in-block").hide();
-        	});
-        };
-        var submitLogic = function ()
-        {
-        	var data = {
-        		email: $("#email").val(),
-        		password: $("#password").val(),
-        		rememberMe: true
-        	};
-        	$.ajax({
-        		type: "POST",
-        		url: "/api/Login",
-        		contentType: "application/json",
-        		data: data ? JSON.stringify(data) : null,
-        		datatype: "json"
-        	}).done(function (data)
-        	{
-        		alert("Logged in successfully!\nReturned: '" + data + "'");
-        		loginLabels();
-        	});
-        };
-        THYF.hideLoading();
+
+	var loginLabels = function()
+	{
+		$.ajax({
+			type: "GET",
+			url: "/api/Login",
+			contentType: "application/json",
+			datatype: "json"
+		}).done(function(user)
+		{
+			$("#logged-in").text("User:" + user.name);
+			$("#logged-in-block").show();
+			$("#notlogged-in-block").hide();
+		});
+	};
+
+	var submit = function()
+	{
+		var data = {
+			email: $("#email").val(),
+			password: $("#password").val(),
+			rememberMe: true
+		};
+		$.ajax({
+			type: "POST",
+			url: "/api/Login",
+			contentType: "application/json",
+			data: data ? JSON.stringify(data) : null,
+			datatype: "json"
+		}).done(function(data)
+		{
+			alert("Logged in successfully!\nReturned: '" + data + "'");
+			loginLabels();
+		});
+	};
+
+	THYF.hideLoading();
 });
