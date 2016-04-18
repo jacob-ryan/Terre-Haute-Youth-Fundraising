0eba63957ba371bb4cb2206cdc1d6507d28e9d7a
Jacob Ryan <ryanjm@rose-hulman.edu>
2016-04-18 16:31:44 -0400

    Fixed bugs on Admin Panel, relocated and included JavaScript files

--- THYF/THYF WebAPI/site/js/admin-home.js
+++ THYF/THYF WebAPI/site/js/admin-home.js
@@ -1,194 +1,217 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function()
+{
 
-    THYF.hideLoading();
-    var userData = [];
-    var table;
-    var paypaltable
-    var checkedValues;
-    var paypalData;
-    var eventList = [];
+	THYF.hideLoading();
+	var userData = [];
+	var table;
+	var paypaltable
+	var checkedValues;
+	var eventList = [];
 
-    // <summary>
-    // Get's all event's from database and populates the event selector within the
-    // Events tab
-    // </summary>
-     $.ajax({
-        type: "GET",
-        url: "/api/EventOccurrence",
-        contentType: "application/json",
-    }).done(function (d) {
-        //console.log(d);
-        eventList = d;
-        $.each(d, function (key, value) {
-            $('#selectEvent')
+	// <summary>
+	// Get's all event's from database and populates the event selector within the
+	// Events tab
+	// </summary>
+	$.ajax({
+		type: "GET",
+		url: "/api/EventOccurrence",
+		contentType: "application/json",
+	}).done(function(d)
+	{
+		//console.log(d);
+		eventList = d;
+		$.each(d, function(key, value)
+		{
+			$('#selectEvent')
                 .append($("<option></option>")
                 .attr("value", value.id)
                 .text(value.type + " " + value.date));
-        });
-    });
-    //console.log(eventList);
+		});
+	});
+	//console.log(eventList);
 
-    // <summary>
-    // Creates the user table within the Users tab
-    // </summary>
-    $.ajax({
-        type: "GET",
-        url: "/api/User",
-        contentType: "application/json",
-    }).done(function (d) {
-        var userArray = [];
-        //console.log(d);
-        for (i = 0; i < d.length; i++) {
-            userData[i] = [];
-            if (d[i].companyName === null || d[i].companyName == "") {
-                d[i].companyName = "No Company";
-            }
-            userData[i].push("<input class = 'boxes' type='checkbox' value='" + d[i].id + "'>", d[i].id, d[i].name, d[i].isActive + "", d[i].email, d[i].type, d[i].address,
+	// <summary>
+	// Creates the user table within the Users tab
+	// </summary>
+	$.ajax({
+		type: "GET",
+		url: "/api/User",
+		contentType: "application/json",
+	}).done(function(d)
+	{
+		var userArray = [];
+		//console.log(d);
+		for (i = 0; i < d.length; i++)
+		{
+			userData[i] = [];
+			if (d[i].companyName === null || d[i].companyName == "")
+			{
+				d[i].companyName = "No Company";
+			}
+			userData[i].push("<input class = 'boxes' type='checkbox' value='" + d[i].id + "'>", d[i].id, d[i].name, d[i].isActive + "", d[i].email, d[i].type, d[i].address,
             d[i].city, d[i].state, d[i].zip, d[i].phone, d[i].tshirtSize, d[i].companyName + "", d[i].dateCreated + "");
-        }
-        //console.log(userData);
+		}
+		//console.log(userData);
 
-        table = $('#example').DataTable({
-            "aaData": userData,
-        });
-    });
+		table = $('#example').DataTable({
+			"aaData": userData,
+		});
+	});
 
-    $.ajax({
-        type: "GET",
-        url: "/api/PayPalNotification",
-        contentType: "application/json",
-    }).done(function (d) {
-        for (i = 0; i < d.length; i++) {
-            var row = [];
-            row.push(d.transactionID, "User Email", "User Name", d.dateReceived, d.paymentGross, d.paymentFee, d.paymentStatus);
-            paypalData[i].push(row);
-        }
+	$.ajax({
+		type: "GET",
+		url: "/api/PayPalNotification",
+		contentType: "application/json",
+	}).done(function(d)
+	{
+		var paypalData = [];
+		for (i = 0; i < d.length; i++)
+		{
+			var row = [];
+			var n = d[i];
+			row.push(n.transactionId, "User Email", "User Name", n.dateReceived, n.paymentGross, n.paymentFee, n.paymentStatus);
+			paypalData.push(row);
+		}
 
-        paypaltable = $('#paypal').DataTable({
-            "aaData": paypalData,
-        });
-    });
+		paypaltable = $('#paypal').DataTable({
+			"aaData": paypalData,
+		});
+	});
 
-    // <summary>
-    // Deactivates all the users selected within Users tab
-    // </summary>
-    $("#deactivate").on("click", function () {
-        checkedValues = $('input:checkbox:checked').map(function () {
-            return this.value;
-        }).get()
+	// <summary>
+	// Deactivates all the users selected within Users tab
+	// </summary>
+	$("#deactivate").on("click", function()
+	{
+		checkedValues = $('input:checkbox:checked').map(function()
+		{
+			return this.value;
+		}).get()
 
-        for (i = 0; i < checkedValues.length; i++) {
-            //console.log(i);
-            makeAPICalls(checkedValues[i], false);
-        }
-    });
+		for (i = 0; i < checkedValues.length; i++)
+		{
+			//console.log(i);
+			makeAPICalls(checkedValues[i], false);
+		}
+	});
 
-    // <summary>
-    // Activates all the users selected within Users tab
-    // </summary>
-    $("#activate").on("click", function () {
-        checkedValues = $('input:checkbox:checked').map(function () {
-            return this.value;
-        }).get()
+	// <summary>
+	// Activates all the users selected within Users tab
+	// </summary>
+	$("#activate").on("click", function()
+	{
+		checkedValues = $('input:checkbox:checked').map(function()
+		{
+			return this.value;
+		}).get()
 
-        for (i = 0; i < checkedValues.length; i++) {
-            //console.log(i);
-            makeAPICalls(checkedValues[i], true);
-        }
-    });
-
-
-    // <summary>
-    // Allows admin to create a new user within the Users tab
-    // </summary>
-    $("#addUser").on("click", function () {
-        var userName = $("#userName").val();
-        var userEmail = $("#userEmail").val();
-
-        var data = {
-            email: userName,
-            newPassword: "change",
-            name: userName,
-            address: "change",
-            city: "change",
-            state: "IN",
-            zip: "47804",
-            phone: "change",
-            year: "change",
-            tshirtSize: "change",
-            type: "volunteer",
-            companyName: "change",
-            isActive: true
-        };
-
-        //console.log(data);
-        $.ajax({
-            type: "POST",
-            url: "/api/User",
-            contentType: "application/json",
-            data: data ? JSON.stringify(data) : null,
-            datatype: "json"
-        }).done(function (data) {
-            alert("User Created" + data + "'");
-        }).fail(function () {
-            alert("Failed to Create User");
-        });
-    });
+		for (i = 0; i < checkedValues.length; i++)
+		{
+			//console.log(i);
+			makeAPICalls(checkedValues[i], true);
+		}
+	});
 
 
-    // <summary>
-    // Allows admin to create a new event within the Events tab
-    // </summary>
-    $("#createEventButton").on("click", function () {
-        var data = {
-            date: $("#datepickerID").val(),
-            type: $("#EventType").val(),
-            description: $("#description").val()
-        };
+	// <summary>
+	// Allows admin to create a new user within the Users tab
+	// </summary>
+	$("#addUser").on("click", function()
+	{
+		var userName = $("#userName").val();
+		var userEmail = $("#userEmail").val();
 
-        console.log(data);
+		var data = {
+			email: userName,
+			newPassword: "change",
+			name: userName,
+			address: "change",
+			city: "change",
+			state: "IN",
+			zip: "47804",
+			phone: "change",
+			year: "change",
+			tshirtSize: "change",
+			type: "volunteer",
+			companyName: "change",
+			isActive: true
+		};
 
-        $.ajax({
-            type: "POST",
-            url: "/api/EventOccurrence",
-            contentType: "application/json",
-            data: data ? JSON.stringify(data) : null,
-            datatype: "json"
-        }).done(function (data) {
-            console.log(data);
-        });
-    });
+		//console.log(data);
+		$.ajax({
+			type: "POST",
+			url: "/api/User",
+			contentType: "application/json",
+			data: data ? JSON.stringify(data) : null,
+			datatype: "json"
+		}).done(function(data)
+		{
+			alert("User Created" + data + "'");
+		}).fail(function()
+		{
+			alert("Failed to Create User");
+		});
+	});
 
-    // <summary>
-    // Populates existing event information when user click Edit Event
-    // within the Events tab
-    // </summary>
-    $("#editEventButton").on("click", function () {
-        $("#descriptionEvent").val(eventList[$("#selectEvent").val() - 1].description);
-        $("#datepickerID2").val(eventList[$("#selectEvent").val() - 1].date);
-    });
 
-    $("#updateEventButton").on("click", function () {
-        var eventId = $("#selectEvent").val();       
-        var data = {
-            date: $("#datepickerID2").val(),
-            type: eventList[eventId - 1].type,
-            description: $("#descriptionEvent").val()
-        };
+	// <summary>
+	// Allows admin to create a new event within the Events tab
+	// </summary>
+	$("#createEventButton").on("click", function()
+	{
+		var data = {
+			date: $("#datepickerID").val(),
+			type: $("#EventType").val(),
+			description: $("#description").val()
+		};
 
-        console.log(data);
-        console.log(eventId);
+		console.log(data);
 
-        $.ajax({
-            type: "PUT",
-            url: "/api/EventOccurrence/" + eventId,
-            contentType: "application/json",
-            data: data ? JSON.stringify(data) : null,
-            datatype: "json"
-        }).fail(function () {
-            alert("Failed To Update Event");
-        });
-    });
+		$.ajax({
+			type: "POST",
+			url: "/api/EventOccurrence",
+			contentType: "application/json",
+			data: data ? JSON.stringify(data) : null,
+			datatype: "json"
+		}).done(function(data)
+		{
+			console.log(data);
+		});
+	});
+
+	// <summary>
+	// Populates existing event information when user click Edit Event
+	// within the Events tab
+	// </summary>
+	$("#editEventButton").on("click", function()
+	{
+		$("#descriptionEvent").val(eventList[$("#selectEvent").val() - 1].description);
+		$("#datepickerID2").val(eventList[$("#selectEvent").val() - 1].date);
+	});
+
+	$("#updateEventButton").on("click", function()
+	{
+		var eventId = $("#selectEvent").val();
+		var data = {
+			date: $("#datepickerID2").val(),
+			type: eventList[eventId - 1].type,
+			description: $("#descriptionEvent").val()
+		};
+
+		console.log(data);
+		console.log(eventId);
+
+		$.ajax({
+			type: "PUT",
+			url: "/api/EventOccurrence/" + eventId,
+			contentType: "application/json",
+			data: data ? JSON.stringify(data) : null,
+			datatype: "json"
+		}).fail(function()
+		{
+			alert("Failed To Update Event");
+		});
+	});
 
 });
 
@@ -196,35 +219,40 @@
 // An extracted method API that is used to activate and deactivate
 // users with in User Tab
 // </summary>
-function makeAPICalls(userID, activateBoolean) {
-    //console.log(userID);
-    $.ajax({
-        type: "GET",
-        url: "/api/User/" + userID,
-        contentType: "application/json",
-        datatype: "json"
-    }).done(function (data) {
-        var userInfo = data;
-        userInfo.isActive = activateBoolean;
-        //console.log(userID);
-        $.ajax({
-            type: "PUT",
-            url: "/api/User/" + userID,
-            contentType: "application/json",
-            data: data ? JSON.stringify(data) : null,
-            datatype: "json"
-        }).done(function (data) {
-            location.reload();
-        });
-    }).fail(function () {
-        alert("No user with that ID");
-    });
+function makeAPICalls(userID, activateBoolean)
+{
+	//console.log(userID);
+	$.ajax({
+		type: "GET",
+		url: "/api/User/" + userID,
+		contentType: "application/json",
+		datatype: "json"
+	}).done(function(data)
+	{
+		var userInfo = data;
+		userInfo.isActive = activateBoolean;
+		//console.log(userID);
+		$.ajax({
+			type: "PUT",
+			url: "/api/User/" + userID,
+			contentType: "application/json",
+			data: data ? JSON.stringify(data) : null,
+			datatype: "json"
+		}).done(function(data)
+		{
+			location.reload();
+		});
+	}).fail(function()
+	{
+		alert("No user with that ID");
+	});
 }
 
 
 // <summary>
 // Refreshs information when event is selected with Events tab
 // </summary>
-function jsFunction() {
-    //console.log("It works");
+function jsFunction()
+{
+	//console.log("It works");
 }
