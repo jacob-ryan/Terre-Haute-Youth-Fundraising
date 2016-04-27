dfc2532adb0e677183a721774e985ff7c50f0b34
Jacob Ryan <ryanjm@rose-hulman.edu>
2016-04-26 20:04:02 -0400

    Fixed Contact Us form submission error, added validation and loading/success feedback, fixed missing Font Awesome icons

--- THYF/THYF WebAPI/site/js/contact-us.js
+++ THYF/THYF WebAPI/site/js/contact-us.js
@@ -1,25 +1,60 @@
 ﻿$(document).ready(function()
 {
-	$("#submit").on("click", function(e)
+	var submit = function()
 	{
+		THYF.showLoading();
+
 		var data = {
-			firstName: $("#fname").val(),
-			lastName: $("#lname").val(),
-			emailAddress: $("#email").val(),
-			phone: $("#phone").val(),
-			message: $("#message").val()
+			firstName: $("#contact-first-name").val(),
+			lastName: $("#contact-last-name").val(),
+			emailAddress: $("#contact-email").val(),
+			phone: $("#contact-phone").val(),
+			message: $("#contact-message").val()
 		};
-		console.log(data);
-//		$.ajax({
-//			type: "POST",
-//			url: "/api/ContactUs",
-//			contentType: "application/json",
-//			data: data ? JSON.stringify(data) : null,
-//			datatype: "json"
-//		}).done(function(data)
-//		{
-//			alert("Information sent!");
-//		});
+
+		$.ajax({
+			type: "POST",
+			url: "/api/ContactUs",
+			contentType: "application/json",
+			data: data ? JSON.stringify(data) : null,
+			datatype: "json"
+		}).done(function()
+		{
+			alert("Your contact request was received successfully.");
+			THYF.go("/");
+		});
+	};
+
+	$("#contact-form").validate(
+	{
+		submitHandler: function()
+		{
+			submit();
+		},
+		rules: {
+			"contact-first-name": {
+				required: true,
+				maxlength: 255
+			},
+			"contact-last-name": {
+				required: true,
+				maxlength: 255
+			},
+			"contact-email": {
+				required: true,
+				maxlength: 255,
+				email: true
+			},
+			"contact-phone": {
+				required: false,
+				maxlength: 23 // 23 characters represents longest form, e.g.: "1 (123) 456-7890 ext. 1234"
+			},
+			"contact-message": {
+				required: true,
+				minlength: 10,
+				maxlength: 255
+			}
+		}
 	});
 
 	THYF.hideLoading();
