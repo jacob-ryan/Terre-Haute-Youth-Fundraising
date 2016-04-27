e7e18af381a93fc69240911f555789ec76c453d6
johade777 <patterda@rose-hulman.edu>
2016-04-27 12:32:35 -0400

    added field checking to update info

--- THYF/THYF WebAPI/site/js/account-edit.js
+++ THYF/THYF WebAPI/site/js/account-edit.js
@@ -1,104 +1,144 @@
-﻿$(document).ready(function()
-{
-	var data; 
-	$.ajax({
-		type: "GET",
-		url: "/api/Login",
-		contentType: "application/json",
-		datatype: "json"
-	}).done(function(user)
-	{
-	    $("#newPass").val(user.password);
-	    $("#email").val(user.email);
-		$("#name").val(user.name);
-		$("#address").val(user.address);
-		$("#city").val(user.city);
-		$("#state").val(user.state);
-		$("#zip").val(user.zip);
-		$("#phone").val(user.phone);
-		$("#year").val(user.year);
-		$("#t-shirt").val(user.tshirtSize);
-		$("#company").val(user.companyName);
-		data = user;
-		console.log(data);
+﻿$(document).ready(function () {
+    var data;
+    $.ajax({
+        type: "GET",
+        url: "/api/Login",
+        contentType: "application/json",
+        datatype: "json"
+    }).done(function (user) {
+        $("#newPass").val(user.password);
+        $("#email").val(user.email);
+        $("#name").val(user.name);
+        $("#address").val(user.address);
+        $("#city").val(user.city);
+        $("#state").val(user.state);
+        $("#zip").val(user.zip);
+        $("#phone").val(user.phone);
+        $("#year").val(user.year);
+        $("#t-shirt").val(user.tshirtSize);
+        $("#company").val(user.companyName);
+        data = user;
+       // console.log(data);
 
-		if(user.companyName == ""){
-			$("#volunteer_sec").show();
-			$("#company_sec").hide();
-		}else{
-			$("#volunteer_sec").hide();
-			$("#company_sec").show();
-		}
-		THYF.hideLoading();
-	});
+        if (user.companyName == "") {
+            $("#volunteer_sec").show();
+            $("#company_sec").hide();
+        } else {
+            $("#volunteer_sec").hide();
+            $("#company_sec").show();
+        }
+        THYF.hideLoading();
+    });
 
-	$("#update").on("click", function(e)
-	{
-		e.preventDefault();
-		console.log("Button Pressed");
-		var newPass = $("#password").val();
-		var newPassCheck = $("#passwordCheck").val();
-		var newName = $("#name").val();
-		var newEmail = $("#email").val();
-		var newAddress = $("#address").val();
-		var newCity = $("#city").val();
-		var newState = $("#state").val();
-		var newZip = $("#zip").val();
-		var newPhone = $("#phone").val();
-		var newSize = $("#t-shirt").val();
-		var newCompany = $("#company").val();
+    $("#updateForm").validate({
+        rules: {
+            "email": {
+                required: true,
+                maxlength: 255,
+                email: true
+            },
+            "password": {
+                required: true,
+                maxlength: 255
+            },
+            "password-check": {
+                required: true,
+                maxlength: 255
+            },
+            "name": {
+                required: true,
+                maxlength: 255
+            },
+            "address": {
+                required: true,
+                maxlength: 255
+            },
+            "city": {
+                required: true,
+                maxlength: 255
+            },
+            "state": {
+                required: true,
+                maxlength: 2
+            },
+            "zip": {
+                required: true,
+                maxlength: 5
+            },
+            "phone": {
+                required: true,
+                maxlength: 23 // 23 characters represents longest form, e.g.: "1 (123) 456-7890 ext. 1234"
+            },
+            "year": {
+                required: true,
+                maxlength: 255
+            },
+            "type": {
+                required: true,
+                maxlength: 255
+            },
+            "t-shirt": {
+                required: false,
+                maxlength: 255
+            },
+            "company_name": {
+                required: false,
+                maxlength: 255
+            }
+        }
+    });
 
-		if (newPass != newPassCheck)
-		{
-			var newPassElement = document.getElementById("password");
-			var newPassCheckElement = document.getElementById("passwordCheck");
-			newPassElement.style.backgroundColor = "#C0C0C0";
-			newPassCheckElement.style.backgroundColor = "#C0C0C0";
-			alert("New Passwords do no match!");
-		} else
-		{
-			if (newPass == "")
-			{
-				data.newPassword = null;
-			} else
-			{
-				data.newPassword = newPass;
-			}
-			alert("Passwords Matched!");
-		}
+    $("#update").on("click", function (e) {
+        e.preventDefault();
+        $("#updateForm").valid();
+        console.log("Button Pressed");
+        var newPass = $("#password").val();
+        var newPassCheck = $("#passwordCheck").val();
+        var newName = $("#name").val();
+        var newEmail = $("#email").val();
+        var newAddress = $("#address").val();
+        var newCity = $("#city").val();
+        var newState = $("#state").val();
+        var newZip = $("#zip").val();
+        var newPhone = $("#phone").val();
+        var newSize = $("#t-shirt").val();
+        var newCompany = $("#company").val();
 
-		data.name = newName
-		data.email = newEmail;
-		data.address = newAddress
-		data.city = newCity;
-		data.state = newState;
-		data.zip = newZip;
-		data.phone = newPhone;
-		data.tshirtSize = newSize;
-		data.company = newCompany;
+        if (newPass != newPassCheck) {
+            var newPassElement = document.getElementById("password");
+            var newPassCheckElement = document.getElementById("passwordCheck");
+            newPassElement.style.backgroundColor = "#C0C0C0";
+            newPassCheckElement.style.backgroundColor = "#C0C0C0";
+            alert("New Passwords do no match!");
+        } else if (isEmptyOrSpaces(newPass)) {
+            alert("Password cannot be blank!");
+        } else {
+            data.name = newName
+            data.email = newEmail;
+            data.address = newAddress
+            data.city = newCity;
+            data.state = newState;
+            data.zip = newZip;
+            data.phone = newPhone;
+            data.tshirtSize = newSize;
+            data.company = newCompany;
 
-		console.log(data);
+            console.log(data);
 
-		$.ajax({
-			type: "PUT",
-			url: "/api/User/" + data.id,
-			contentType: "application/json",
-			data: data ? JSON.stringify(data) : null,
-			datatype: "json"
-		}).done(function(data)
-		{
+            $.ajax({
+                type: "PUT",
+                url: "/api/User/" + data.id,
+                contentType: "application/json",
+                data: data ? JSON.stringify(data) : null,
+                datatype: "json"
+            }).done(function (data) {
+               $("#logged-in").html("<span class='badge badge-default'>" + newName + "</span>");
+               alert("Information Updated!\nReturned: '" + data + "'");
+            });
+        }
+    });
 
-		    $.ajax({
-		        type: "PUT",
-		        url: "/api/User/" + data.id,
-		        contentType: "application/json",
-		        data: data ? JSON.stringify(data) : null,
-		        datatype: "json"
-		    }).done(function (data) {
-		        $("#logged-in").html("<span class='badge badge-default'>" + newName + "</span>");
-		        alert("Information Updated!\nReturned: '" + data + "'");
-		    });
-
-		});
-	});
+    function isEmptyOrSpaces(str) {
+        return str === null || str.match(/^ *$/) !== null;
+    }
 });
